a8075f6aa94c264a3e39b5ca494ec28b0d729b85
Dhaval <dhaval@dhaval-laptop.(none)>
2011-09-04 12:27:38 -0400

    Initial commit

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+
+   def index 
+      @users_list = User.all || []
+   end
+   
+   def create
+      begin
+         @user_info = {
+            :name => params[:name],
+            :email_address => params[:email_address],
+            :password => params[:password]
+         }
+         
+         User.create @user_info
+      rescue
+         Rails.logger "[User Controller] Couldn't create the user"
+      end
+      redirect_to '/users'
+   end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project1::Application.config.secret_token = 'f3946f643283ee1b02eb605a6d9a8545548349798bc848d5dc027c72e85c2182d65be22521aba8fb07fe12bab6ee8a5fbd5149160d331ec48a0db09820aa61b5'
